9cc7aafa83a15179bbc47bc8f94f2016479a1f28
EVERTON MOSCALESKI <91913754+Moska75leski@users.noreply.github.com>
2024-05-14 19:28:56 -0300

    Tela Cadastre/Email

--- lib/app.dart
+++ lib/app.dart
@@ -1,12 +1,13 @@
+import 'package:exekevistaapp/pages/cadastre.dart';
 import 'package:flutter/material.dart';
-// import 'package:exekevistaapp/pages/login.dart';
-import 'package:exekevistaapp/pages/home.dart';
+import 'package:exekevistaapp/pages/login.dart';
+//import 'package:exekevistaapp/pages/home.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Home();
+    return const Cadastre();
   }
 }
